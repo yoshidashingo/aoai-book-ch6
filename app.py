c110a2f320bb94a6873faaa543ce08b2fedaa14d
Shingo <yoshidashingo@users.noreply.github.com>
2023-10-10 18:52:59 +0900

    Update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import (
     HumanMessage,
@@ -39,12 +40,24 @@ def chat():
             def on_llm_end(self, response, **kwargs):
                 queue_obj.put('[DONE]')
 
-        chat = ChatOpenAI(model=os.getenv('OPENAI_API_MODEL'), 
-                        streaming=True,
-                        temperature=os.getenv('OPENAI_TEMPERATURE'), 
-                        callbacks=[StreamCallbackHandler()], 
-                        model_kwargs={"deployment_id":os.getenv('AZURE_DEPLOYMENT_ID')}
-                        )
+        # chat = ChatOpenAI(model=os.getenv('OPENAI_API_MODEL'), 
+        #                 streaming=True,
+        #                 temperature=os.getenv('OPENAI_TEMPERATURE'), 
+        #                 callbacks=[StreamCallbackHandler()], 
+        #                 model_kwargs={"deployment_id":os.getenv('AZURE_DEPLOYMENT_ID')}
+        #                 )
+
+        chat = AzureChatOpenAI(
+            deployment_name=os.getenv('AZURE_DEPLOYMENT_ID'),
+            openai_api_type=openai.api_type,
+            openai_api_base=openai.api_base,
+            openai_api_version=openai.api_version,
+            openai_api_key=openai.api_key,
+            streaming=True,
+            temperature=os.getenv('OPENAI_TEMPERATURE'),
+            callbacks=[StreamCallbackHandler()]
+        )
+
 
         messages = [
             SystemMessage(content="")
--- static/js/app.js
+++ static/js/app.js
@@ -11,6 +11,7 @@ function replaceURLWithHTMLLinks(text) {
 }
 
 function sendPrompt(prompt) {
+  response.innerHTML = "";
   keyword.value = "";
   keyword.placeholder = "処理中...";
   keyword.disabled = true;
@@ -23,33 +24,30 @@ function sendPrompt(prompt) {
   };
 }
 
+function reset() {
+  keyword.placeholder = "何でも聞いてください...";
+  keyword.disabled = false;
+  keyword.focus();
+}
+
 function insertAnswer(answer, prompt) {
   if (answer === "[DONE]") {
     reset();
-    g(CHAT_ID).innerHTML = replaceURLWithHTMLLinks(g(CHAT_ID).innerHTML);
+    response.innerHTML = replaceURLWithHTMLLinks(response.innerHTML);
     return;
   }
-  const card = g(CHAT_ID);
+  const card = response;
   if (card) {
     card.innerText += answer;
   } else {
-    let html = `<div class="card answer">${answer}</div>`;
-    content.insertAdjacentHTML("beforeend", html);
+    let html = `${answer}`;
     response.insertAdjacentHTML("beforeend", html);
   }
 }
 
-
-
 // DEFINE DOM ELEMENTS
 const response = g("response");
-const content = g("content");
 const keyword = g("keyword");
-const header = g("header");
-const tip = g("tip");
-const close = g("close");
-const mockChatBody = g("mock-chat-body");
-const chatBody = g("chat-body");
 
 keyword.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
