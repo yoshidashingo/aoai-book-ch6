682f8a44651ee89e0ac3dfbeca2157e46515e81a
Shingo <yoshidashingo@users.noreply.github.com>
2023-11-06 17:58:15 +0900

    update

--- app.py
+++ app.py
@@ -11,15 +11,13 @@ app = Flask(__name__)
 
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
-openai.api_type = 'azure'
-openai.api_version = '2023-05-15'
+openai.api_type = os.getenv("OPENAI_API_TYPE")
+openai.api_version = os.getenv("AZURE_OPENAI_VERSION")
 deployment_id = os.getenv("AZURE_DEPLOYMENT_ID")
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/chat')
 def chat():
